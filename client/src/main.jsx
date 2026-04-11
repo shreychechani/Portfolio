29afa5de26c4f6d9c5a4b4313469ccfce365905f
shrey_chechani <shreychechani@gmail.com>
2026-04-11 21:24:58 +0530

    updated main.jsx file code and applied the wraped sun/moon mode on whole app function some error with adding main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
+import 'aos/dist/aos.css'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// ThemeProvider wraps everything so any component can access dark/light mode
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 )
