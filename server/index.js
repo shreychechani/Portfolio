391eb253ac0d88ba305e3a8b0ec1aa6ada59784a
shrey_chechani <shreychechani@gmail.com>
2026-04-12 21:25:57 +0530

    re deloyed due to error

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const __dirname  = path.dirname(__filename)
 
 app.use(cors({
   origin: [
-    'https://portfolio-lyart-chi-39.vercel.app',
+    'https://portfolio-client-red.vercel.app',
     'http://localhost:5173',
     'http://localhost:4173',
   ],
